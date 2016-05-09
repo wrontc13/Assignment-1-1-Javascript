968c35bc465558416b9a28ca60b1257c0b6db145
wrontc13 <wrontc13@gmail.com>
2016-05-08 20:24:04 -0400

    Added files via upload

---
+++ circle-area.js
@@ -0,0 +1,11 @@
+﻿function circlearea(r) {
+    return r * 3.14;
+}
+
+var div = document.getElementById('data')
+var div1 = document.getElementById('data1')
+var div2 = document.getElementById('data2')
+div.textContent = circlearea(7)
+div1.textContent = circlearea(1.5)
+div2.textContent = circlearea(20)
+
